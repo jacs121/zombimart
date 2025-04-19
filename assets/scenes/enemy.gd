1dabbd131025efe090edc8ecfe3b3b00c7b8d182
jacs121 <101277107+jacs121@users.noreply.github.com>
2025-04-19 12:29:45 +0300

    enemy sight fix
    fixed enemy line of sight

--- assets/scenes/enemy.gd
+++ assets/scenes/enemy.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-@export var speed : int
-@export var sight : float
+@export var speed : float = 300.0
+@export var sight : float = 2000.0
+@export var slow_down_sight : float = 150.0
 
 var player_pos
 var target_pos
@@ -15,8 +16,13 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	player_pos = player.position
 	target_pos = (player_pos - position).normalized()
-	
-	if position.distance_to(player_pos) > sight:
+
+	if position.distance_to(player_pos) <= slow_down_sight:
+		velocity.x = target_pos.x * speed * 0.8
+		velocity.y = target_pos.y * speed * 0.8
+		move_and_slide()
+
+	elif position.distance_to(player_pos) <= sight:
 		velocity.x = target_pos.x * speed
 		velocity.y = target_pos.y * speed
 		move_and_slide()
