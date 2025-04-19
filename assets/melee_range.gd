49824428f9f80bdf041f6493ff9c71e23500b5b3
Trombonia <idolevin2007@gmail.com>
2025-04-19 18:57:32 +0300

    tried to fix stuff

--- assets/melee_range.gd
+++ assets/melee_range.gd
@@ -4,5 +4,6 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		var mousePos = get_global_mouse_position()
 		var player : CharacterBody2D = get_parent().get_parent()
-		rotation_degrees = rad_to_deg(((mousePos.y - player.position.y)/(mousePos.x - player.position.y)))
+		
+		rotation_degrees = rad_to_deg(atan((mousePos.y - player.position.y)/(mousePos.x - player.position.y)))
 		print(rotation_degrees)
